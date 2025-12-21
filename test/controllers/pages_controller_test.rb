ff092f4927ad094b66592fb2e002fe98ad8f6726
Лужняк Александр <Lyzhniak2005@yandex.ru>
2025-12-21 22:45:15 +0300

    Тесты

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get dashboard" do
-    get pages_dashboard_url
+  def setup
+    @user = create_test_user
+  end
+
+  test "should get profile when logged in" do
+    # Логиним пользователя
+    log_in_as(@user)
+
+    get profile_path
     assert_response :success
+  end
+
+  test "should redirect profile when not logged in" do
+    get profile_path
+    assert_redirected_to login_path
+    assert_equal "Пожалуйста, войдите в систему", flash[:alert]
   end
 end
